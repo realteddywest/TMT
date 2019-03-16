63f4cf0a0b246fb91992fa45c7fa5d35e9dcf433
realteddywest <teddytasslakova@gmail.com>
2019-03-16 10:40:53 +0200

    Remove directives

--- TMT1/TMT1/Models/Criminal.cs
+++ TMT1/TMT1/Models/Criminal.cs
@@ -4,7 +4,6 @@ namespace TMT1
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
 
     public partial class Criminal
     {
