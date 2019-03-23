2bf10ed362186b56c126ec22621dae17e9a41b9f
Temenujka <temenujka_211@abv.bg>
2019-03-23 10:39:18 +0200

    Edited method GrowAgent and edited some mistakes in Agents' data

---
+++ TMT1/TMT1/Controllers/UpdateController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMT1.Controllers
+{
+    public class UpdateController
+    {
+        private TMTContext context;
+        public UpdateController(TMTContext context)
+        {
+            this.context = context;
+        }
+
+        public bool UpdateAgentNickname(int id, string newNickname)
+        {
+            var agent = context.Agents.FirstOrDefault(a => a.Id == id);
+            if (agent != null)
+            {
+                agent.Nickname = newNickname;
+                context.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public bool GrwoAgent(int id)
+        {
+            var agent = context.Agents.FirstOrDefault(a => a.Id == id);
+            if (agent != null)
+            {
+                agent.Age++;
+                context.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public bool UpdateCriminalStatus(int id, string newStatus)
+        {
+            var criminal = context.Criminals.FirstOrDefault(c => c.Id == id);
+            if (criminal != null)
+            {
+                criminal.Status = newStatus;
+               context.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool UpdateCriminalSentence(int id, string newSentence)
+        {
+            var criminal = context.Criminals.FirstOrDefault(c => c.Id == id);
+            if (criminal != null)
+            {
+                criminal.Sentence = newSentence;
+                context.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
