ffe8212364eb159798bc8f8acf92a4df4af26686
Temenujka <temenujka_211@abv.bg>
2019-03-24 10:51:29 +0200

    Added new FormReadCriminalInfo.Designer.cs

--- TMT1/TMT1/Forms/FormReadCriminalInfo.Designer.cs
+++ TMT1/TMT1/Forms/FormReadCriminalInfo.Designer.cs
@@ -37,6 +37,19 @@
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.labelCriminalId = new System.Windows.Forms.Label();
+            this.labelCriminalName = new System.Windows.Forms.Label();
+            this.labelCriminalNickname = new System.Windows.Forms.Label();
+            this.labelCriminalCrime = new System.Windows.Forms.Label();
+            this.labelCriminalSentence = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.CriminalID = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -103,7 +116,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label7.Location = new System.Drawing.Point(16, 224);
+            this.label7.Location = new System.Drawing.Point(16, 235);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(56, 20);
             this.label7.TabIndex = 6;
@@ -113,7 +126,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label8.Location = new System.Drawing.Point(16, 254);
+            this.label8.Location = new System.Drawing.Point(16, 272);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(117, 20);
             this.label8.TabIndex = 7;
@@ -123,17 +136,160 @@
             // 
             this.labelCriminalId.AutoSize = true;
             this.labelCriminalId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelCriminalId.Location = new System.Drawing.Point(182, 30);
+            this.labelCriminalId.Location = new System.Drawing.Point(221, 22);
             this.labelCriminalId.Name = "labelCriminalId";
             this.labelCriminalId.Size = new System.Drawing.Size(51, 20);
             this.labelCriminalId.TabIndex = 8;
             this.labelCriminalId.Text = "label9";
+            this.labelCriminalId.Click += new System.EventHandler(this.labelCriminalId_Click);
+            // 
+            // labelCriminalName
+            // 
+            this.labelCriminalName.AutoSize = true;
+            this.labelCriminalName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCriminalName.Location = new System.Drawing.Point(221, 54);
+            this.labelCriminalName.Name = "labelCriminalName";
+            this.labelCriminalName.Size = new System.Drawing.Size(51, 20);
+            this.labelCriminalName.TabIndex = 9;
+            this.labelCriminalName.Text = "label9";
+            this.labelCriminalName.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // labelCriminalNickname
+            // 
+            this.labelCriminalNickname.AutoSize = true;
+            this.labelCriminalNickname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCriminalNickname.Location = new System.Drawing.Point(221, 89);
+            this.labelCriminalNickname.Name = "labelCriminalNickname";
+            this.labelCriminalNickname.Size = new System.Drawing.Size(51, 20);
+            this.labelCriminalNickname.TabIndex = 10;
+            this.labelCriminalNickname.Text = "label9";
+            this.labelCriminalNickname.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // labelCriminalCrime
+            // 
+            this.labelCriminalCrime.AutoSize = true;
+            this.labelCriminalCrime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCriminalCrime.Location = new System.Drawing.Point(221, 156);
+            this.labelCriminalCrime.Name = "labelCriminalCrime";
+            this.labelCriminalCrime.Size = new System.Drawing.Size(51, 20);
+            this.labelCriminalCrime.TabIndex = 12;
+            this.labelCriminalCrime.Text = "label9";
+            // 
+            // labelCriminalSentence
+            // 
+            this.labelCriminalSentence.AutoSize = true;
+            this.labelCriminalSentence.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCriminalSentence.Location = new System.Drawing.Point(221, 191);
+            this.labelCriminalSentence.Name = "labelCriminalSentence";
+            this.labelCriminalSentence.Size = new System.Drawing.Size(51, 20);
+            this.labelCriminalSentence.TabIndex = 13;
+            this.labelCriminalSentence.Text = "label9";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(211, 121);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 16;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(211, 227);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 28);
+            this.comboBox2.TabIndex = 17;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(211, 271);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 28);
+            this.comboBox3.TabIndex = 18;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(30, 415);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(79, 24);
+            this.checkBox1.TabIndex = 19;
+            this.checkBox1.Text = "Search";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(30, 324);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 47);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(211, 324);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 47);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(211, 403);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 47);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(211, 496);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 24;
+            // 
+            // CriminalID
+            // 
+            this.CriminalID.AutoSize = true;
+            this.CriminalID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CriminalID.Location = new System.Drawing.Point(26, 499);
+            this.CriminalID.Name = "CriminalID";
+            this.CriminalID.Size = new System.Drawing.Size(82, 20);
+            this.CriminalID.TabIndex = 25;
+            this.CriminalID.Text = "CriminalID";
             // 
             // FormReadCriminalInfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(412, 435);
+            this.ClientSize = new System.Drawing.Size(484, 555);
+            this.Controls.Add(this.CriminalID);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.labelCriminalSentence);
+            this.Controls.Add(this.labelCriminalCrime);
+            this.Controls.Add(this.labelCriminalNickname);
+            this.Controls.Add(this.labelCriminalName);
             this.Controls.Add(this.labelCriminalId);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
@@ -145,6 +301,7 @@
             this.Controls.Add(this.label1);
             this.Name = "FormReadCriminalInfo";
             this.Text = "FormReadCriminalInfo";
+            this.Load += new System.EventHandler(this.FormReadCriminalInfo_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -161,5 +318,18 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label labelCriminalId;
+        private System.Windows.Forms.Label labelCriminalName;
+        private System.Windows.Forms.Label labelCriminalNickname;
+        private System.Windows.Forms.Label labelCriminalCrime;
+        private System.Windows.Forms.Label labelCriminalSentence;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label CriminalID;
     }
 }
--- TMT1/TMT1/Forms/FormReadCriminalInfo.cs
+++ TMT1/TMT1/Forms/FormReadCriminalInfo.cs
@@ -16,5 +16,35 @@ namespace TMT1.Forms
         {
             InitializeComponent();
         }
+
+        private void labelCriminalId_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormReadCriminalInfo_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelCriminalStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
