23ecc88844cfc31d61849b1976cbb1929d713668
Mariya <mariqmalcheva@abv.bg>
2019-03-24 12:21:36 +0200

    Added new FormReadCriminalInfo.Designer.cs

---
+++ TMT1/TMT1/Forms/FormReadCriminalInfo.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace TMT1.Forms
+{
+    partial class FormReadCriminalInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CriminalID = new System.Windows.Forms.Label();
+            this.Name = new System.Windows.Forms.Label();
+            this.NickName = new System.Windows.Forms.Label();
+            this.TownId = new System.Windows.Forms.Label();
+            this.Crime = new System.Windows.Forms.Label();
+            this.Sentence = new System.Windows.Forms.Label();
+            this.Status = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EvilnessFactor = new System.Windows.Forms.Label();
+            this.labelCriminalID = new System.Windows.Forms.Label();
+            this.labelCriminalName = new System.Windows.Forms.Label();
+            this.labelCriminalNickName = new System.Windows.Forms.Label();
+            this.labelCriminalTownId = new System.Windows.Forms.Label();
+            this.labelCriminalCrime = new System.Windows.Forms.Label();
+            this.labelCriminalSentence = new System.Windows.Forms.Label();
+            this.labelCriminalStatus = new System.Windows.Forms.Label();
+            this.labelCriminalEvilnessFactor = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CriminalID
+            // 
+            this.CriminalID.AutoSize = true;
+            this.CriminalID.Location = new System.Drawing.Point(68, 14);
+            this.CriminalID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.CriminalID.Name = "CriminalID";
+            this.CriminalID.Size = new System.Drawing.Size(82, 20);
+            this.CriminalID.TabIndex = 0;
+            this.CriminalID.Text = "CriminalID";
+            this.CriminalID.Click += new System.EventHandler(this.CriminalID_Click);
+            // 
+            // Name
+            // 
+            this.Name.AutoSize = true;
+            this.Name.Location = new System.Drawing.Point(68, 68);
+            this.Name.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(51, 20);
+            this.Name.TabIndex = 1;
+            this.Name.Text = "Name";
+            // 
+            // NickName
+            // 
+            this.NickName.AutoSize = true;
+            this.NickName.Location = new System.Drawing.Point(48, 123);
+            this.NickName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.NickName.Name = "NickName";
+            this.NickName.Size = new System.Drawing.Size(81, 20);
+            this.NickName.TabIndex = 2;
+            this.NickName.Text = "NickName";
+            this.NickName.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // TownId
+            // 
+            this.TownId.AutoSize = true;
+            this.TownId.Location = new System.Drawing.Point(48, 176);
+            this.TownId.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.TownId.Name = "TownId";
+            this.TownId.Size = new System.Drawing.Size(61, 20);
+            this.TownId.TabIndex = 3;
+            this.TownId.Text = "TownId";
+            // 
+            // Crime
+            // 
+            this.Crime.AutoSize = true;
+            this.Crime.Location = new System.Drawing.Point(48, 223);
+            this.Crime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Crime.Name = "Crime";
+            this.Crime.Size = new System.Drawing.Size(50, 20);
+            this.Crime.TabIndex = 4;
+            this.Crime.Text = "Crime";
+            this.Crime.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Sentence
+            // 
+            this.Sentence.AutoSize = true;
+            this.Sentence.Location = new System.Drawing.Point(48, 272);
+            this.Sentence.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Sentence.Name = "Sentence";
+            this.Sentence.Size = new System.Drawing.Size(78, 20);
+            this.Sentence.TabIndex = 5;
+            this.Sentence.Text = "Sentence";
+            // 
+            // Status
+            // 
+            this.Status.AutoSize = true;
+            this.Status.Location = new System.Drawing.Point(48, 320);
+            this.Status.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(56, 20);
+            this.Status.TabIndex = 6;
+            this.Status.Text = "Status";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(343, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 7;
+            // 
+            // EvilnessFactor
+            // 
+            this.EvilnessFactor.AutoSize = true;
+            this.EvilnessFactor.Location = new System.Drawing.Point(48, 373);
+            this.EvilnessFactor.Name = "EvilnessFactor";
+            this.EvilnessFactor.Size = new System.Drawing.Size(113, 20);
+            this.EvilnessFactor.TabIndex = 8;
+            this.EvilnessFactor.Text = "EvilnessFactor";
+            // 
+            // labelCriminalID
+            // 
+            this.labelCriminalID.AutoSize = true;
+            this.labelCriminalID.Location = new System.Drawing.Point(269, 14);
+            this.labelCriminalID.Name = "labelCriminalID";
+            this.labelCriminalID.Size = new System.Drawing.Size(14, 20);
+            this.labelCriminalID.TabIndex = 9;
+            this.labelCriminalID.Text = "-";
+            // 
+            // labelCriminalName
+            // 
+            this.labelCriminalName.AutoSize = true;
+            this.labelCriminalName.Location = new System.Drawing.Point(269, 68);
+            this.labelCriminalName.Name = "labelCriminalName";
+            this.labelCriminalName.Size = new System.Drawing.Size(14, 20);
+            this.labelCriminalName.TabIndex = 10;
+            this.labelCriminalName.Text = "-";
+            // 
+            // labelCriminalNickName
+            // 
+            this.labelCriminalNickName.AutoSize = true;
+            this.labelCriminalNickName.Location = new System.Drawing.Point(269, 123);
+            this.labelCriminalNickName.Name = "labelCriminalNickName";
+            this.labelCriminalNickName.Size = new System.Drawing.Size(14, 20);
+            this.labelCriminalNickName.TabIndex = 11;
+            this.labelCriminalNickName.Text = "-";
+            // 
+            // labelCriminalTownId
+            // 
+            this.labelCriminalTownId.AutoSize = true;
+            this.labelCriminalTownId.Location = new System.Drawing.Point(269, 176);
+            this.labelCriminalTownId.Name = "labelCriminalTownId";
+            this.labelCriminalTownId.Size = new System.Drawing.Size(14, 20);
+            this.labelCriminalTownId.TabIndex = 12;
+            this.labelCriminalTownId.Text = "-";
+            // 
+            // labelCriminalCrime
+            // 
+            this.labelCriminalCrime.AutoSize = true;
+            this.labelCriminalCrime.Location = new System.Drawing.Point(269, 223);
+            this.labelCriminalCrime.Name = "labelCriminalCrime";
+            this.labelCriminalCrime.Size = new System.Drawing.Size(14, 20);
+            this.labelCriminalCrime.TabIndex = 13;
+            this.labelCriminalCrime.Text = "-";
+            // 
+            // labelCriminalSentence
+            // 
+            this.labelCriminalSentence.AutoSize = true;
+            this.labelCriminalSentence.Location = new System.Drawing.Point(269, 272);
+            this.labelCriminalSentence.Name = "labelCriminalSentence";
+            this.labelCriminalSentence.Size = new System.Drawing.Size(14, 20);
+            this.labelCriminalSentence.TabIndex = 14;
+            this.labelCriminalSentence.Text = "-";
+            // 
+            // labelCriminalStatus
+            // 
+            this.labelCriminalStatus.AutoSize = true;
+            this.labelCriminalStatus.Location = new System.Drawing.Point(269, 320);
+            this.labelCriminalStatus.Name = "labelCriminalStatus";
+            this.labelCriminalStatus.Size = new System.Drawing.Size(14, 20);
+            this.labelCriminalStatus.TabIndex = 15;
+            this.labelCriminalStatus.Text = "-";
+            // 
+            // labelCriminalEvilnessFactor
+            // 
+            this.labelCriminalEvilnessFactor.AutoSize = true;
+            this.labelCriminalEvilnessFactor.Location = new System.Drawing.Point(269, 373);
+            this.labelCriminalEvilnessFactor.Name = "labelCriminalEvilnessFactor";
+            this.labelCriminalEvilnessFactor.Size = new System.Drawing.Size(14, 20);
+            this.labelCriminalEvilnessFactor.TabIndex = 16;
+            this.labelCriminalEvilnessFactor.Text = "-";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 431);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 49);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(209, 431);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 46);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(209, 497);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 47);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(44, 520);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(106, 24);
+            this.checkBox1.TabIndex = 20;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 566);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(264, 26);
+            this.textBox1.TabIndex = 21;
+            // 
+            // FormReadCriminalInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelCriminalEvilnessFactor);
+            this.Controls.Add(this.labelCriminalStatus);
+            this.Controls.Add(this.labelCriminalSentence);
+            this.Controls.Add(this.labelCriminalCrime);
+            this.Controls.Add(this.labelCriminalTownId);
+            this.Controls.Add(this.labelCriminalNickName);
+            this.Controls.Add(this.labelCriminalName);
+            this.Controls.Add(this.labelCriminalID);
+            this.Controls.Add(this.EvilnessFactor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.Sentence);
+            this.Controls.Add(this.Crime);
+            this.Controls.Add(this.TownId);
+            this.Controls.Add(this.NickName);
+            this.Controls.Add(this.Name);
+            this.Controls.Add(this.CriminalID);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FormReadCriminalInfo";
+            this.Text = "FormReadCriminalInfo";
+            this.Load += new System.EventHandler(this.FormReadCriminalInfo_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label CriminalID;
+        private System.Windows.Forms.Label Name;
+        private System.Windows.Forms.Label NickName;
+        private System.Windows.Forms.Label TownId;
+        private System.Windows.Forms.Label Crime;
+        private System.Windows.Forms.Label Sentence;
+        private System.Windows.Forms.Label Status;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label EvilnessFactor;
+        private System.Windows.Forms.Label labelCriminalID;
+        private System.Windows.Forms.Label labelCriminalName;
+        private System.Windows.Forms.Label labelCriminalNickName;
+        private System.Windows.Forms.Label labelCriminalTownId;
+        private System.Windows.Forms.Label labelCriminalCrime;
+        private System.Windows.Forms.Label labelCriminalSentence;
+        private System.Windows.Forms.Label labelCriminalStatus;
+        private System.Windows.Forms.Label labelCriminalEvilnessFactor;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
