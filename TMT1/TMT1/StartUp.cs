2e24fbd9b0187606b747e7b058f37c00f11a98e7
Mariya <mariqmalcheva@abv.bg>
2019-03-29 17:49:02 +0200

    modify StartUp.cs

--- TMT1/TMT1/StartUp.cs
+++ TMT1/TMT1/StartUp.cs
@@ -13,18 +13,21 @@ namespace TMT1
     {
         static void Main(string[] args)
         {
-           //ConsoleController controller = new ConsoleController();
+           ConsoleController controller = new ConsoleController();
 
             TMTContext context = new TMTContext();
-            DeleteController d = new DeleteController(context);
-            UpdateController u = new UpdateController(context);
+            // DeleteController d = new DeleteController(context);
+            //  UpdateController u = new UpdateController(context);
             // u.UpdateCriminalStatus(9, "Dead");
             // u.UpdateCriminalSentence(9, "15 life terms of imprisonment - 957 years total");
             //u.UpdateAgentNickname(13, "Coco");
 
+            
             Application.EnableVisualStyles();
+           // Application.Run(new FormReadCriminalInfo());
+           // Application.Run(new FormReadTownInfo());
 
-           Application.Run(new FormReadCriminalInfo());
+         //  Application.Run(new FormReadCriminalInfo());
             
         }
     }
