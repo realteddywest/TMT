d6d18bf43e713db3a1e2655bea1506af77a39f13
Mariya <mariqmalcheva@abv.bg>
2019-04-01 12:03:50 +0300

    create FormAddNewAgent.cs

--- TMT1/TMT1/Forms/FormAddNewAgent.Designer.cs
+++ TMT1/TMT1/Forms/FormAddNewAgent.Designer.cs
@@ -35,8 +35,10 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
@@ -101,13 +103,6 @@
             this.textBox3.Size = new System.Drawing.Size(236, 26);
             this.textBox3.TabIndex = 6;
             // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(255, 171);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(236, 26);
-            this.textBox4.TabIndex = 7;
-            // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
@@ -117,14 +112,45 @@
             this.button1.TabIndex = 8;
             this.button1.Text = "ADD";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(46, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Town ID";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(255, 166);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(236, 28);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(255, 203);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(236, 28);
+            this.comboBox2.TabIndex = 11;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // FormAddNewAgent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(533, 348);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -151,7 +177,9 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
--- TMT1/TMT1/Forms/FormAddNewAgent.cs
+++ TMT1/TMT1/Forms/FormAddNewAgent.cs
@@ -7,19 +7,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TMT1.Controllers;
 
 namespace TMT1.Forms
 {
     public partial class FormAddNewAgent : Form
     {
-        public FormAddNewAgent()
+        private TMTContext context;
+        private InsertController insert;
+        private ReadController read;
+        private List<Country> countries;
+        private List<Town> towns;
+        public FormAddNewAgent(TMTContext context, InsertController insert, ReadController read)
         {
             InitializeComponent();
+            this.context = context;
+            this.insert = insert;
+            this.read = read;
         }
 
         private void FormAddNewAgent_Load(object sender, EventArgs e)
         {
-            
+            countries = read.CountriesList();
+            towns = read.TownsList();
+            foreach (var t in towns)
+            {
+                comboBox1.Items.Add(t.Name);
+            }
+            foreach (var c in countries)
+            {
+                comboBox2.Items.Add(c.Name);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool isAdd = insert.InsertIntoAgents(textBox1.Text, textBox2.Text, int.Parse(textBox3.Text), comboBox1.Text, comboBox2.Text);
+            if (isAdd)
+            {
+                MessageBox.Show("Agent is added!");
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.Text = towns.FirstOrDefault(c => c.Name == comboBox1.Text).Country.Name;
+            comboBox2.Enabled = false;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
