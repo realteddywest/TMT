a22c64247c7d958d1838f5da67d4400a33f868ac
Temenujka <temenujka_211@abv.bg>
2019-04-01 12:08:57 +0300

    Create Forms/FormMain.Designer.cs

---
+++ TMT1/TMT1/Forms/FormMain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TMT1.Forms
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
