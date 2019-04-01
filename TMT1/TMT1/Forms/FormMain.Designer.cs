23b8efea23bdcd37f492b219bde679a78f0f2573
Mariya <mariqmalcheva@abv.bg>
2019-04-01 12:13:59 +0300

    Create Forms/FormMain.cs

--- TMT1/TMT1/Forms/FormMain.Designer.cs
+++ TMT1/TMT1/Forms/FormMain.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -47,15 +47,6 @@
             this.groupBox1.Text = "Add";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(43, 55);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 43);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(43, 117);
@@ -64,6 +55,16 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // FormMain
             // 
--- TMT1/TMT1/Forms/FormMain.cs
+++ TMT1/TMT1/Forms/FormMain.cs
@@ -7,19 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TMT1.Controllers;
 
 namespace TMT1.Forms
 {
     public partial class FormMain : Form
     {
-        public FormMain()
+        private TMTContext context;
+        private InsertController insert;
+        private ReadController read;
+        private UpdateController update;
+        private DeleteController delete;
+        public FormMain(TMTContext context, InsertController insert, ReadController read, UpdateController update, DeleteController delete)
         {
             InitializeComponent();
+            this.context = context;
+            this.insert = insert;
+            this.read = read;
+            this.update = update;
+            this.delete = delete;
         }
 
         private void groupBox1_Enter(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form form = new FormAddNewAgent(context, insert, read);
+            form.Show();
         }
     }
 }
