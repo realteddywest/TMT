c11a1ac509c3534661007928560abd78de542e11
Temenujka <temenujka_211@abv.bg>
2019-04-02 10:45:34 +0300

    Modify

--- TMT1/TMT1/StartUp.cs
+++ TMT1/TMT1/StartUp.cs
@@ -16,22 +16,6 @@ namespace TMT1
       
             ConsoleController controller = new ConsoleController();
 
-            //TMTContext context = new TMTContext();
-           
-            // DeleteController d = new DeleteController(context);
-            //  UpdateController u = new UpdateController(context);
-            // u.UpdateCriminalStatus(9, "Dead");
-            // u.UpdateCriminalSentence(9, "15 life terms of imprisonment - 957 years total");
-            //u.UpdateAgentNickname(13, "Coco");
-
-            
-           // Application.EnableVisualStyles();
-            // Application.Run(new FormReadCriminalInfo());
-            //  Application.Run(new FormReadAgentInfo());
-           // Application.Run(new FormReadTownInfo());
-            //  Application.Run(new FormReadCriminalInfo());
-         // Application.Run(new FormAddNewCriminal());
-
         }
     }
 }
